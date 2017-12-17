de2b331e3f8ba57e979d92170407b3f781fd0d49
hutingung <hutingung@gmail.com>
2017-12-17 09:03:52 +0800

    Commit holiday-tools and intuit-integration (partial)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Membership';
+   constructor(private oauthService: OAuthService) {
+                // Login-Url
+        this.oauthService.loginUrl = "https://steyer-identity-server.azurewebsites.net/identity/connect/authorize"; //Id-Provider?
+
+        // URL of the SPA to redirect the user to after login
+        this.oauthService.redirectUri = window.location.origin + "/index.html";
+
+        // The SPA's id. Register SPA with this id at the auth-server
+        this.oauthService.clientId = "Q0yqMqBBvMu5eoRjXNmhiNp0dQRzSPtUGHTNPt6rNqzzVR5L9H";
+
+        // set the scope for the permissions the client should request
+        this.oauthService.scope = "openid profile email voucher";
+
+        // Use setStorage to use sessionStorage or another implementation of the TS-type Storage
+        // instead of localStorage
+        this.oauthService.setStorage(sessionStorage);
+
+        // To also enable single-sign-out set the url for your auth-server's logout-endpoint here
+        this.oauthService.logoutUrl = "https://steyer-identity-server.azurewebsites.net/identity/connect/endsession";
+
+        // This method just tries to parse the token(s) within the url when
+        // the auth-server redirects the user back to the web-app
+        // It dosn't send the user the the login page
+        this.oauthService.tryLogin();      
+        this.oauthService.requireHttps=false;
+  }
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,20 +2,17 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { OAuthService } from 'angular-oauth2-oidc';
 
 @Component({
   selector: 'app-login',
-  template: `
-  <div> {{ (user | async)?.uid }} </div>
-  <button (click)="login()">Login</button>
-  <button (click)="logout()">Logout</button>
-  `,
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
 
   user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
+  constructor(public afAuth: AngularFireAuth, private oAuthService: OAuthService) {
     this.user = afAuth.authState;
     console.log("user:", afAuth);
   }
@@ -27,4 +24,17 @@ export class LoginComponent {
   logout() {
     this.afAuth.auth.signOut();
   }
+  
+  public loginIntuit() {
+       this.oAuthService.initImplicitFlow();
+  }
+    
+  public logoutIntuit() {
+        this.oAuthService.logOut();
+  }
+  
+  public get name() {
+    return 'a';
+  }
+    
 }
